89ef6a420da8482df550fbc4d1f07580aefa1d52
Sakamoto Z <smapendoo@gmail.com>
2026-09-14 22:21:51 -0400

    Update test.js

--- Movement-system/test.js
+++ Movement-system/test.js
@@ -1,3 +1,14 @@
-const leftbtn = document.getElementById("left")
-const rightbtn = document.getElementById("right")
-const 
+
+const leftbtn = document.getElementById("left");
+const rightbtn = document.getElementById("right");
+const ball = document.getElementById("ball");
+
+let posX = 9; /* changeable */
+
+leftbtn.onclick = function() {
+ball.style.left -= 10 + "px";
+};
+
+rightbtn.onclick = function() {
+ball.style.left += 10 + "px";
+};
